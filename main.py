ba308dda4f95b161529517dfbf9645855be4adc4
TheImagoDei <joseiowusu538@gmail.com>
2026-03-28 19:10:52 +0000

    seg2seg model optimzed

--- main.py
+++ main.py
@@ -5,52 +5,67 @@ import pandas as pd
 import numpy as np
 
 # creating an seq2seq model
-class lstmencoderModel(nn.modules):
-    def __init__(self, input_dim, hidden_dim):
+class lstmencoderModel(nn.module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super().__init__()
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
 
-    def forward(self, X):
-        
-        output, (hidden, cell) = self.lstm(X)
+    def forward(self, x):
+
+        x = self.embedding(x)
+        output, (hidden, cell) = self.lstm(x)
 
         return hidden, cell
     
 
-class decoderModel(nn.modules):
-    def __init__(self, output_dim, hidden_dim):
+class decoderModel(nn.module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super().__init__()
-        self.lstm = nn.LSTM(output_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
 
     def forward(self, x, hidden, cell):
+        x = self.embedding(x)
         output, (hidden, cell) = self.lstm(x, (hidden, cell))
         prediction = self.fc(output)
         return prediction, hidden, cell
     
 
-class seq2seqModel(nn.modules):
-    def __init__(self, encoder, decoder):
-        super.__init__()
+class seq2seqModel(nn.Module):
+    def __init__(self, encoder, decoder, vocab_size):
+        super().__init__()
+
         self.encoder = encoder
         self.decoder = decoder
+        self.vocab_size = vocab_size
 
     def forward(self, src, trg, teacher_forcing_ratio=0.5):
-        batch_size, trg_len, output_dim = trg.shape
+        # src: (batch, src_len)
+        # trg: (batch, trg_len)
 
-        outputs = torch.zeros(batch_size, trg_len, output_dim)
+        batch_size, trg_len = trg.shape
+
+        outputs = torch.zeros(batch_size, trg_len, self.vocab_size)
 
         hidden, cell = self.encoder(src)
 
-        input = trg[:, 0:1, :] # first token
+        # first token (<sos>)
+        input = trg[:, 0]   # (batch,)
 
         for t in range(1, trg_len):
+            input = input.unsqueeze(1)  # (batch, 1)
+
             output, hidden, cell = self.decoder(input, hidden, cell)
+
             outputs[:, t:t+1, :] = output
 
             teacher_force = torch.rand(1).item() < teacher_forcing_ratio
 
-            input = trg[:, t:t+1, :] if teacher_force else output
+            top1 = output.argmax(2).squeeze(1)
+
+            input = trg[:, t] if teacher_force else top1
 
         return outputs
 
